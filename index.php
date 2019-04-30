ed8d7bace093766f6c1e26f7e9a2ac9a0087e923
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-01 00:53:00 +0700

    new

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
 $sql = "SELECT * FROM product";
 $db = parse_url(getenv("DATABASE_URL"));
 $pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    "host=ec2-54-221-236-144.compute-1.amazonaws.com;port=5432;user=tkxdkzzzureptd
+	;password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453;dbname=
+	d621ll97foi9ku",
     $db["host"],
     $db["port"],
     $db["user"],
