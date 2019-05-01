3a839743ec57da49ba006cd7d9a9a12483459ca4
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-02 00:40:54 +0700

    new

---
+++ Searching.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.searchbar{
+			float: right;
+			padding : 25px 5px 0px 0px;
+
+		}
+	</style>
+	<title>ATN shop</title>
+	
+</head>
+<body>
+	<form action="Searching.php" class="searchbar" method="GET">
+			<input type="text" placeholder="Search.." name="search" >
+  			<button type="submit"><i class="fa fa-search"></i></button>
+	</form>
+	<div>
+		<?php 
+$sql = "SELECT * FROM product where productName like '%".$_GET['search']."%'";
+$db = parse_url(getenv("DATABASE_URL"));
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=ec2-54-221-236-144.compute-1.amazonaws.com;port=5432;user=tkxdkzzzureptd
+	;password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453;dbname=
+	d621ll97foi9ku",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
+echo "<table id = 'table'>";
+	echo  "<tr>";
+	echo "<th>Image</th>";
+	echo "<th>Product name</th>";
+	echo "<th>Product Description</th>";
+	echo "<th>Price</th>";
+	echo "<th>Quantity</th>";
+	echo "</tr>";
+foreach ($resultSet as $row) {
+	echo "<tr>";
+	echo "<td>";?><div><img src="/<?php echo $row['image']; ?>"></div><?php "</td>";
+	echo "<td>" . $row['productname'] . "</td>";
+	echo "<td>" . $row['productdescription'] . "</td>";
+	echo "<td>" . $row['price'] . "</td>";
+	echo "<td>" . $row['quantity'] . "</td>"; 
+	echo "</tr>";
+}
+	echo "</table>";
+?>
+	</div>
+</body>
+</html>
