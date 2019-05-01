f5ff852d9c824ead5d4f58ffb94b876121fec26a
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-01 18:14:39 +0700

    new

--- index.php
+++ index.php
@@ -1,42 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
 	<title>ATN shop</title>
-
 </head>
 <body>
-	<div style="margin: auto">
-		<?php 
-$sql = "SELECT * FROM product";
-$db = parse_url(getenv("DATABASE_URL"));
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=ec2-54-221-236-144.compute-1.amazonaws.com;port=5432;user=tkxdkzzzureptd
-	;password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453;dbname=
-	d621ll97foi9ku",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
-$stmt = $pdo->prepare($sql);
-//Thiết lập kiểu dữ liệu trả về
-$stmt->setFetchMode(PDO::FETCH_ASSOC);
-$stmt->execute();
-$resultSet = $stmt->fetchAll();
-foreach ($resultSet as $row) {
 
-	?>
-	<div><img src="/<?php echo $row['image']; ?>"></div>
-	<?php 
-	echo "<h1>" . $row['productname'] . "</h1>";
-	echo "<div>" . $row['productdescription'] . "</div>";
-	echo "<div>" . $row['price'] . "</div>";
-	?><div>Quantity: <?php echo $row['quantity']; ?></div><?php
-	
-}
-?>
+	<div>
+		<?php
+		require_once('./dbconnector.php');
+		$conn = new DBconnector();
+		$sql = "select * from product";
+		$rows = $conn -> runQuery($sql);
+		for ($i=0; $i < count($rows); $i++) { 
+			?>
+			<div>
+
+				<div>
+					<a ><img src="<?=$rows[$i]['image']?>" alt=""></a>
+				</div>
+				<div><?=$rows[$i]['productName']?></div>
+				<div><?=$rows[$i]['productDescription']?></div>
+				<div><?=$rows[$i]['price']?></div>
+			</div>
+			<?php
+		}
+		?>
 	</div>
 </body>
 </html>
