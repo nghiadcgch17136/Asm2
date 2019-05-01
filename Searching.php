91ca9d37514388e472c21b46c005855b451b2618
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-02 00:59:55 +0700

    new

--- Searching.php
+++ Searching.php
@@ -4,7 +4,6 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>ATN shop</title>
-	
 </head>
 <body>
 	<form action="Searching.php" class="searchbar" method="GET">
