48bbcc9f752bc6b4f24cc7ad51753d8e3eb2077a
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-01 22:41:16 +0700

    new

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
 	<style>
 		#table{
 			border: 1px solid black;
+			border-collapse: collapse;
 			margin: auto;
 		}
-		
+		.image{
+			width: 80px;
+			height: 150px;
+		}
 	</style>
 	<title>ATN shop</title>
 	<table></table>
