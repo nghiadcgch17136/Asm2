2c363ba63b63fae599439a046b5cac96f74b6cf3
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-01 18:26:38 +0700

    new

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ foreach ($resultSet as $row) {
 	echo "<th>Product Description</th>";
 	echo "<th>Price</th>";
 	echo "<th>Quantity</th>";
-	while ($row = pg_fetch_assoc($resultSet)) {echo "<tr>";
+	while ($row <= count($row)) {echo "<tr>";
 	
 		echo "<td>";?><div><img src="/<?php echo $row['image']; ?>"></div><?php "</td>";
 		echo "<td>" . $row['productname'] . "<td>";
