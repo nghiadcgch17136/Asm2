aa6ef8b3b68c7f1fc0af52ba362e8d6284a3b396
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-03 00:11:55 +0700

    new

--- admin.php
+++ admin.php
@@ -52,7 +52,7 @@
 		if (isset($_GET['del'])) {
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com;port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$sql = "DELETE FROM products WHERE productid=".$_GET['del'];
+		$sql = "DELETE FROM product WHERE productid=".$_GET['del'];
 		$result = pg_query($db,$sql);
 		$row = pg_fetch_assoc($result);
 		}
