e41e441df422f038badfc5d31fcdd4ac72d39409
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-03 00:10:43 +0700

    new

--- admin.php
+++ admin.php
@@ -52,9 +52,9 @@
 		if (isset($_GET['del'])) {
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com;port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$sql = "DELETE FROM products WHERE productid=".$_POST['del'];
+		$sql = "DELETE FROM products WHERE productid=".$_GET['del'];
 		$result = pg_query($db,$sql);
-		$row1 = pg_fetch_assoc($result);
+		$row = pg_fetch_assoc($result);
 		}
 	 ?>
 		<?php 
@@ -91,7 +91,7 @@ foreach ($resultSet as $row) {
 	echo "<td>" . $row['price'] . "</td>";
 	echo "<td>" . $row['quantity'] . "</td>"; 
 	?><td><form action="" method="POST">
-					<button class="function" onclick="return Deleteqry(<?php echo $rows1['productid'] ?>)"><img src="img/rubbish-bin-delete-button.png" alt=""></button></form></td><?php
+					<button class="function" onclick="return Deleteqry(<?php echo $row['productid'] ?>)"><img src="img/rubbish-bin-delete-button.png" alt=""></button></form></td><?php
 	echo "</tr>";
 }
 	echo "</table>";
