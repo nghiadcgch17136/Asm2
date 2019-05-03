b15f2172f3715fa91c4260d9e36f906bab0c4656
Chinh Nghia <nghiadcgch17136@fpt.edu.vn>
2019-05-03 23:23:19 +0700

    new

--- admin.php
+++ admin.php
@@ -36,7 +36,7 @@
             return false;
         }
      	function edit(id) {
-     		window.location="Update.php?id="+id;
+     		window.location="update.php?id="+id;
          
      	}   
 	</script>
@@ -59,6 +59,21 @@
 		
 		}
 	 ?>
+	 <?php 
+		if (isset($_GET['productname'])) {
+		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
+	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
+		$ProductName = $_GET['productname'];	
+		$Image = $_GET['image'];
+		$DescribeProducts = $_GET['productdescription'];
+		$Price = $_GET['price'];
+		$Quantity = $_GET['quantity']
+		$sql = "UPDATE product SET productname='".$ProductName."',productdescription='".$DescribeProducts."',price= '".$Price."',quantity ='".$Quantity."', image='".$Image."' WHERE productid =" .$_GET['productid'];
+		$result = pg_query($db,$sql);
+		
+		}
+	 ?>
+
 		<?php 
 $sql = "SELECT * FROM product";
 $db = parse_url(getenv("DATABASE_URL"));
---
+++ update.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ATN shop</title>
+</head>
+<body>
+	<?php
+	if (isset($_GET['productid'])) {
+		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
+	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
+		$query = "Select * From product where productid =".$_GET['productid'];	
+		$result = pg_query($query);	}
+		
+	?>
+	<?php 
+		while ($row = pg_fetch_assoc($result)) {
+			?><form action="admin.php" method="GET">
+		<table style="margin: 0 auto">
+			<tr>
+				<td>ProductID:</td>
+				<td> <?php echo $_GET['productid'] ?> <input type="hidden" name="productid" value="<?php echo $_GET['productid'] ?>"></td>
+			</tr>
+			<tr>
+				<td>ProductName:</td>
+				<td><input type="text" name="productname" value="<?php echo $r['productname'] ?>"></td>
+			</tr>
+			<tr>
+			<td>Image:</td>
+			<td><input type="text" name="image" value="<?php echo $r['image'] ?>"></td>
+			</tr>
+			<tr>
+			<td>DescribeProducts:</td>
+			<td><input type="text" name="productdescription" value="<?php echo $r['productdescription'] ?>"></td>
+			</tr>
+			<tr>
+			<td>Price:</td>
+			<td><input type="text" name="price" value="<?php echo $r['price'] ?>"></td>
+			</tr>
+		}
+	?>
+</body>
+</html>
