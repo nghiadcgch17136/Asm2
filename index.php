93fb1ebeb396bad9ec1be4f1a38c53a13d95e066
Chinh Nghia <nghiadcgch17136@fpt.edu.vn>
2019-05-03 20:51:47 +0700

    new

--- index.php
+++ index.php
@@ -153,17 +153,17 @@ span.psw {
 
 <div id="id01" class="modal">
   
-  <form class="modal-content animate" action="login.php" method="POST">
+  <form class="modal-content animate" action= method="POST">
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
       <img src="img/icon.png" alt="Avatar" class="avatar">
     </div>
 
     <div class="container">
-      <label for="uname"><b>Username</b></label>
+      <label for="user"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="user" required>
 
-      <label for="psw"><b>Password</b></label>
+      <label for="pass"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="pass" required>
         
       <button type="submit">Login</button>
@@ -174,7 +174,7 @@ span.psw {
 
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
+      
     </div>
   </form>
 </div>
--- login.php
+++ login.php
@@ -1,16 +1,15 @@
-
-	<?php
+<?php
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
 		$query = "SELECT * FROM account WHERE username = '$_POST[user]' and password = '$_POST[pass]'";	
-			$result = pg_query($query);
+		$result = pg_query($query);
 		$count = pg_num_rows($result)
 		if ($count == 1) {
-			# code...
+			
 			header("location:admin.php");
 		}
 		else {
-			# code...
+			
 			header("location:index.php");
 		}
 	?>
