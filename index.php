a6a5d3aecc83d86533f70f74c650255bba8cc34d
Chinh Nghia <nghiadcgch17136@fpt.edu.vn>
2019-05-03 15:10:58 +0700

    new

--- index.php
+++ index.php
@@ -149,7 +149,7 @@ span.psw {
 	<title>ATN shop</title>
 </head>
 <body>
-	<button onclick="document.getElementById('id01').style.display='block'" style="float: left;">Login</button>
+	<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
 <div id="id01" class="modal">
   
@@ -174,7 +174,7 @@ span.psw {
 
     <div class="container" style="background-color:#f1f1f1">
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      
+      <span class="psw">Forgot <a href="#">password?</a></span>
     </div>
   </form>
 </div>
