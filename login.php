35e6589ebe2ac402d22e70edc9831d66773095af
Chinh Nghia <nghiadcgch17136@fpt.edu.vn>
2019-05-03 15:48:49 +0700

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,8 @@
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
 		$sql = "SELECT DISTINCT username from account where username = ".$_GET['uname'];
 		$result = pg_query($db,$sql);
-		if ($result == 1) {
+		$count = pg_num_rows($result)
+		if ($count == 1) {
 			# code...
 			header('Location:admin.php');
 		}
