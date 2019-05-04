54856335d9204a37e9e5d53a7c5f2527d7675b3b
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-04 17:20:26 +0700

    new

--- update.php
+++ update.php
@@ -5,11 +5,14 @@
 </head>
 <body>
 		<?php
-			$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
+		if (isset($_POST['productid'])) {
+		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$query = "";	
+		$query = "SELECT * FROM product where productid = ";	
 		$result = pg_query($db,$query);
 		?>
+		}
+			
 		<table style="margin: auto;">
 		<tr>
 			<td>Product name:</td>
