2c73a9201c6ab58a18386f401df4f520e0bde19d
Chinh Nghia <nghiadcgch17136@fpt.edu.vn>
2019-05-04 07:52:26 +0700

    new

--- update.php
+++ update.php
@@ -8,7 +8,7 @@
 	if (isset($_GET['productid'])) {
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$query = "Select * From product where productid =".$_GET['productid'];	
+		$query = "SELECT * FROM product WHERE productid =".$_GET['productid'];	
 		$result = pg_query($query);	}
 		
 	?>
@@ -22,19 +22,19 @@
 			</tr>
 			<tr>
 				<td>ProductName:</td>
-				<td><input type="text" name="productname" value="<?php echo $r['productname'] ?>"></td>
+				<td><input type="text" name="productname" value="<?php echo $row['productname'] ?>"></td>
 			</tr>
 			<tr>
 			<td>Image:</td>
-			<td><input type="text" name="image" value="<?php echo $r['image'] ?>"></td>
+			<td><input type="text" name="image" value="<?php echo $row['image'] ?>"></td>
 			</tr>
 			<tr>
 			<td>DescribeProducts:</td>
-			<td><input type="text" name="productdescription" value="<?php echo $r['productdescription'] ?>"></td>
+			<td><input type="text" name="productdescription" value="<?php echo $row['productdescription'] ?>"></td>
 			</tr>
 			<tr>
 			<td>Price:</td>
-			<td><input type="text" name="price" value="<?php echo $r['price'] ?>"></td>
+			<td><input type="text" name="price" value="<?php echo $row['price'] ?>"></td>
 			</tr>
 		}
 	?>
