16f0b9c00b1a8c982bf619a08265d138327d9f91
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-04 18:03:37 +0700

    Update update.php

--- update.php
+++ update.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 		<?php
-		if (isset($_POST['productid'])) {
+		if (isset($_GET['productid'])) {
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$query = "UPDATE product SET productname = '$_POST[productname]',productdescription = '$_POST[productdescription]',price = '$_POST[price]', quantity = '$_POST[quantity]', image = '$_POST[image]' where productid = '$_POST[productid]'";	
-		$sql = "SELECT * FROM product where productid ='$_POST[productid]'";
+		$query = "UPDATE product SET productname = '$_POST[productname]',productdescription = '$_POST[productdescription]',price = '$_POST[price]', quantity = '$_POST[quantity]', image = '$_POST[image]' where productid = '$_GET[productid]'";	
+		$sql = "SELECT * FROM product where productid ='$_GET[productid]'";
 		$result = pg_query($db,$query);
 		$results = pg_query($db,$sql);
 
@@ -18,11 +18,11 @@
 
 		
 			
-		<form action="admin.php" method="POST"><table style="margin: auto;">
+		<form action="admin.php" method="GET"><table style="margin: auto;">
 		<tr>
-			<?$row=pg_fetch_assoc($results)?>
-			<td>ProductID: <?php echo $row['productid'] ?></td>
-			<td><input type="hidden" name="productid" value="<?php echo $row['productid'] ?>"></td>
+			
+			<td>ProductID: <?php echo $_GET['productid'] ?></td>
+			<td><input type="hidden" name="productid" value="<?php echo $_GET['productid'] ?>"></td>
 		</tr>
 		<tr>
 			<td>Product name:</td>
