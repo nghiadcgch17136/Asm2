8655952570ddc2ad52266461eef64727791dad34
Chinh Nghia <nghiadcgch17136@fpt.edu.vn>
2019-05-04 08:02:19 +0700

    new

--- update.php
+++ update.php
@@ -6,11 +6,23 @@
 <body>
 	<?php
 	if (isset($_GET['productid'])) {
-		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
-	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$query = "SELECT * FROM product WHERE productid =".$_GET['productid'];	
-		$result = pg_query($query);	}
-		
+		$sql = "SELECT * FROM product WHERE productid =".$_GET['productid'];
+		$db = parse_url(getenv("DATABASE_URL"));
+		$pdo = new PDO("pgsql:" . sprintf(
+    "host=ec2-54-221-236-144.compute-1.amazonaws.com;port=5432;user=tkxdkzzzureptd
+	;password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453;dbname=
+	d621ll97foi9ku",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+$stmt = $pdo->prepare($sql);
+//Thiết lập kiểu dữ liệu trả về
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$stmt->execute();
+$resultSet = $stmt->fetchAll();
 	?>
 	<?php 
 		while ($row = pg_fetch_assoc($result)) {
@@ -35,6 +47,10 @@
 			<tr>
 			<td>Price:</td>
 			<td><input type="text" name="price" value="<?php echo $row['price'] ?>"></td>
+			</tr>
+			<tr>
+				<td>Quantity:</td>
+				<td><input type="text" name="quantity" value="<?php echo $row['quantity'] ?>"></td>
 			</tr>
 		</table>
 		}
