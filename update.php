877aac5e3b81085a18840917fc36726318fa13e2
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-04 17:34:06 +0700

    Update update.php

--- update.php
+++ update.php
@@ -9,9 +9,9 @@
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
 		$query = "UPDATE product SET productname = '$_POST[productname]',productdescription = '$_POST[productdescription]',price = '$_POST[price]', quantity = '$_POST[quantity]', image = '$_POST[image]' where productid = '$_POST[productid]'";	
-		$result = pg_query($db,$query);
+		$result = pg_query($db,$query);}
 		?>
-		}
+		
 			
 		<form action="admin.php" method="POST"><table style="margin: auto;">
 		
