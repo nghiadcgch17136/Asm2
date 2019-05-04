ddcbec6b36844de0b5ae9dbc703cb9ba729640c3
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-04 15:08:23 +0700

    e

--- update.php
+++ update.php
@@ -4,18 +4,28 @@
 	<title>ATN shop</title>
 </head>
 <body>
-	<?php
-	if (isset($_GET['productid'])) {
-		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
-	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$sql = "SELECT * FROM products WHERE ProductID =".$_GET['productid'];
-		$result = pg_query($db,$sql);
-	?>
 		<table style="margin: auto;">
 		<tr>
-			<td>ProductID:</td>
-			<td><?php echo $_GET['productid'] ?> <input type="hidden" name="productid" value="<?php echo $_GET['productid'] ?>"></td>
+			<td>Product name:</td>
+			<td><input type="text" name="productname" required></td>
 		</tr>
+		<tr>
+			<td>Product description:</td>
+			<td><input type="text" name="productdescription" required></td>
+		</tr>
+		<tr>
+			<td>Price:</td>
+			<td><input type="text" name="price" required></td>
+		</tr>
+		<tr>
+			<td>Quantity:</td>
+			<td><input type="text" name="quantity" required></td>
+		</tr>
+		<tr>
+			<td>Image:</td>
+			<td><input type="text" name="image"></td>
+		</tr>
+
 	</table>
 	}
 	?>
