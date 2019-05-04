ff8bacc0d1620b1cc98fd4eb6957a7a77a9ae51c
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-04 19:07:10 +0700

    Update update.php

--- update.php
+++ update.php
@@ -13,7 +13,7 @@
 
 		
 			
-		<form action="admin.php" method="POST"><table style="margin: auto;">
+		<form action="admin.php" method="GET"><table style="margin: auto;">
 		<tr>
 			
 			<td>ProductID: <?php echo $_GET['productid'] ?></td>
