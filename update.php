f213759201cc0cea48df1d3fbc6a80c5158d03c2
nghiadcgch17136 <49479525+nghiadcgch17136@users.noreply.github.com>
2019-05-04 17:23:05 +0700

    new

--- update.php
+++ update.php
@@ -8,12 +8,16 @@
 		if (isset($_POST['productid'])) {
 		$db = pg_connect("host=ec2-54-221-236-144.compute-1.amazonaws.com port=5432 user=tkxdkzzzureptd
 	 password=597dac569edc9b06099f1027652ff9aab479d2d6c501e71e1c31adc6bd6ed453 dbname=d621ll97foi9ku");
-		$query = "SELECT * FROM product where productid = ";	
+		$query = "UPDATE product SET productname = '$_POST[productname]',productdescription = '$_POST[productdescription]',price = '$_POST[price]', quantity = '$_POST[quantity]', image = '$_POST[image]' where productid = '$_POST[productid]'";	
 		$result = pg_query($db,$query);
 		?>
 		}
 			
-		<table style="margin: auto;">
+		<form action="admin.php" method="POST"><table style="margin: auto;">
+		<tr>
+			<td>ProductID:</td>
+			<td><?php echo $_GET['productid'] ?><input type="hidden" name="productid" value="<?php echo $_GET['productid'] ?>"></td>
+		</tr>
 		<tr>
 			<td>Product name:</td>
 			<td><input type="text" name="productname" required></td>
@@ -36,7 +40,7 @@
 		</tr>
 		<tr><td><input type="submit" value="SAVE"></td></tr>
 
-	</table>
+	</table></form>
 		
 </body>
 </html>
